0afe7968d0e36bdc315441c58ee8d6c7bfd5ff24
naeem <naeemislamjr182@gmail.com>
2023-10-13 04:42:43 +0600

    delete in server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -54,6 +54,16 @@ async function run() {
         app.get('/users', async (req, res) => {
             const cursor = userCollection.find()
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        //delete
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('please delete from database', id);
+            const query = { _id: new ObjectId(id) }
+
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
